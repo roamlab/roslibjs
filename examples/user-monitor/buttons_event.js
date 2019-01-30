a5e5341dd370130e492fb58cac731a132b955216
Long Wang <long.w@columbia.edu>
2019-01-29 23:14:47 -0500

    updated ip adress

--- examples/user-monitor/buttons_event.js
+++ examples/user-monitor/buttons_event.js
@@ -10,8 +10,7 @@ ros.on('close', function() {
 });
 
 // Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:9090');
-
+ros.connect('ws://127.0.0.1:9090');
 // Publishing a Topic
 // ------------------
 

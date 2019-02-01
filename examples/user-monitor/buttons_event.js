831244b5fcbf1aa736b21db6a42e9996d9080a61
Long Wang <long.w@columbia.edu>
2019-01-31 23:36:11 -0500

    in progress - enabling changing camera reference for teleop

--- examples/user-monitor/buttons_event.js
+++ examples/user-monitor/buttons_event.js
@@ -14,15 +14,22 @@ ros.connect('ws://127.0.0.1:9090');
 // Publishing a Topic
 // ------------------
 
-// First, we create a Topic object with details of the topic's name and message type.
+// Topic objects with details of the topic's name and message type.
 var joyButtonsTopic = new ROSLIB.Topic({
   ros : ros,
   name : '/joy',
   messageType : 'sensor_msgs/Joy'
 });
 
-// Then we create the payload to be published. The object we pass in to ros.Message matches the
-// fields defined in the geometry_msgs/Twist.msg definition.
+
+var comdFrameTopic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/joy',
+  messageType : 'sensor_msgs/Joy'
+});
+
+// Messages
+
 var joy_buttons = new ROSLIB.Message({
   header : {
     seq : 1,
